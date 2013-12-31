146b55d4acbd3d410bec024b03583255bceb17a7
Hagen Kuehl <nornagest@gmx.de>
2013-12-31 16:55:40 +0100

    Created rudimentary AnyEvent version of temperature reader for RPi using the DS18B20.

---
+++ temp_anyevent.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+use Modern::Perl 2013;
+use warnings;
+
+use DS18B20;
+
+use AnyEvent;
+
+my $ds18b20 = DS18B20->new();
+
+my $w = AnyEvent->timer (
+    after => 1, 
+    interval => 300, 
+    cb => sub { read_temp(); }
+);
+
+sub read_temp {
+  for(@{$ds18b20->Sensors}) {
+      my %temp = (
+        "time" => scalar localtime(),
+        "sensor" => $_->File,
+        "value" => $_->get_temp(),
+      );
+      process_temp(\%temp);
+  }
+}
+   
+sub process_temp {
+    my $temp = shift;
+    print $temp->{"time"}, " ", $temp->{"sensor"}, " ", $temp->{"value"}, "°C\n";
+}
+
+ 
+my $timer = IO::Async::Timer::Periodic->new(
+   interval => 300,
+   first_interval => 1,
+ 
+   on_tick => sub {
+     read_temp();
+   },
+);
+
+my $exit_loop = AnyEvent->condvar;
+
+$exit_loop->recv; 
