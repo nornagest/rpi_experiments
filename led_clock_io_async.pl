5cd2e94ecd47f3e2c7f0f9e292cea4173760d992
Hagen Kuehl <nornagest@gmx.de>
2014-01-08 23:31:02 +0100

    Some comments and first iteration of clock program;

--- led_clock_io_async.pl
+++ led_clock_io_async.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl 
 #===============================================================================
 #
-#         FILE: buttons_io_async.pl
+#         FILE: led_clock_io_async.pl
 #
-#        USAGE: ./buttons_io_async.pl  
+#        USAGE: ./led_clock_io_async.pl  
 #
-#  DESCRIPTION: 
+#  DESCRIPTION: Show time in binary format with PiFace LEDs, switch between
+#               hour, minute and second display on button press
 #
 #      OPTIONS: ---
 # REQUIREMENTS: ---
@@ -14,9 +15,28 @@
 #       AUTHOR: Hagen Kuehl
 # ORGANIZATION: 
 #      VERSION: 1.0
-#      CREATED: 12/29/2013 12:48:50 PM
+#      CREATED: 01/08/2014 10:59:45 PM
 #     REVISION: ---
 #===============================================================================
+
+#other ideas:
+#display:
+# temperature
+# load
+# output volume
+#
+#change:
+# audio volume
+
+#need to check:
+#DateTime object (again)
+#timer interrupt (Time::HiRes e.g.)
+
+#TODO:
+#keep track of display (hour/min/sec)
+#check every second/100ms if output changes or just output current value
+#indicate current output state (show 1/2/3 on change for a moment)
+#add display of date
 
 use Modern::Perl 2013;
 use warnings;
@@ -25,48 +45,19 @@ use MyPiFace;
 use IO::Async::Channel;
 use IO::Async::Loop;
 use IO::Async::Routine;
+use IO::Async::Timer::Periodic; 
 use Time::HiRes qw(sleep usleep);
 
-#Main:
-#
-# Create PiFace
-# Create Loop
-# Create Notifiers and stuff
-# Start Loop
-#
-#Routine 1:
-# Read PiFace inputs
-# on inputs -> send via Channel
-#
-#Routine 2:
-# on input from channel write to PiFace outputs
-#
-#Signal:
-# handle external Signals
-# Stop/Restart
-#
-#Timer:
-# Check services regularly
-# play/stop on time
-#
-#Stream/Socket
-# for inputs via web interface or keyboard
-#
 #TODO:
-#clean up on exit
-#refactoring
-#extract creation of notifiers, make classes if useful
-#handle keeping state more sensible
-#manage Channels more sensible
-#add more stuff (signals, stream input, services/programs/commands to start)
-
-
-#global state in this case for counter on LEDs
+#use sensible values of DateTime structure or hash for mapping
+#
+#0 - hour
+#1 - minute
+#2 - second
 my $state = 0;
-my $in_ch1 = IO::Async::Channel->new;
+
 my $out_ch1  = IO::Async::Channel->new;
 my $in_ch2 = IO::Async::Channel->new;
-my $out_ch2  = IO::Async::Channel->new;
 
 my $piface = MyPiFace->new;
 
@@ -75,6 +66,8 @@ my $loop = IO::Async::Loop->new;
 create_and_add_notifiers($loop);
 say "Ready...";
 $loop->run;
+
+###########################################
 
 sub start {
     say "Initializing PiFace...";
@@ -91,17 +84,25 @@ sub create_and_add_notifiers {
     my $loop = shift;
     create_input_routine($loop);
     create_output_routine($loop);
+    create_timer($loop);
+}
+sub create_timer {
+    my $loop = shift;
+    my $timer = IO::Async::Timer::Periodic->new(
+        interval => 1,
+        first_interval => 1,
+        on_tick => sub { handle_tick(); },
+    );
+    $loop->add( $timer );
 }
 
 sub create_input_routine {
     my $loop = shift;
 
     my $input_routine = IO::Async::Routine->new(
-        channels_in  => [ $in_ch1 ],
         channels_out => [ $out_ch1 ],
 
         code => sub {
-            $out_ch1->send( { 'text' => "Input routine started..." } );
             start();
 
             my $last_input = 0;
@@ -126,7 +127,6 @@ sub create_input_routine {
         on_recv => sub {
             my ( $ch, $refout ) = @_;
             say "Input routine says: ", $refout->{'text'}
-            if(defined $refout->{'text'});
             if(defined $refout->{'input'} && defined $refout->{'last_input'}) {
                 my $input = $refout->{'input'};
                 my $last_input =  $refout->{'last_input'};
@@ -144,8 +144,6 @@ sub create_output_routine {
         channels_out => [ $out_ch2 ],
 
         code => sub {
-            my $output = "Output routine started...";
-            $out_ch2->send( \$output );
             start();
 
             while(1) {
@@ -160,13 +158,6 @@ sub create_output_routine {
         },
     );
     $loop->add( $output_routine );
-
-    $out_ch2->configure(
-        on_recv => sub {
-            my ( $ch, $output ) = @_;
-            say "Output routine says: $$output";
-        }
-    );
 }
 
 sub handle_input {
@@ -186,19 +177,20 @@ sub handle_button {
     my $button = shift;
 
     if    ($button == 0) { 
-        $state = ($state + 1) % 256;
-        say "+1 ($state)";
+        $state = ($state + 1) % 3;
     } elsif ($button == 1) { 
-        $state = ($state - 1) % 256;
-        say "-1 ($state)";
-    } elsif ($button == 2) { 
+        $state = ($state - 1) % 3;
+    } elsif (button == 2) {
         $state = 0;
-        say "reset. ($state)";
     } elsif ($button == 3) { 
         say "exit.";
         finish();
     };
-
-    $in_ch2->send( \$state );
 }
 
+#react on timer interrupt
+sub handle_tick {
+    #send part of time instead
+    my $time = time() % 256;
+    $in_ch2->send( \$time );
+}
