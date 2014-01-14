c2be24610ac9dca4bcb8a6dd7024b2287e8687c1
Hagen Kühl <nornagest@gmx.de>
2014-01-14 13:44:07 +0100

    Temporary version of a webcam program to regularly take a picture

--- another.pl
+++ another.pl
@@ -71,3 +71,39 @@ for my $i (0..100) {
 
 
 pfio_deinit;
+
+#Here starts the temporary version of a webcam program
+#!/usr/bin/perl
+
+use IO::Async::Loop;
+use IO::Async::Signal;
+use IO::Async::Timer::Periodic;
+
+my $loop = IO::Async::Loop->new;
+my $timer = IO::Async::Timer::Periodic->new(
+	interval => 30,
+	first_interval => 0,
+	on_tick => \&take_pic,
+);
+$timer->start;
+$loop->add($timer);
+my $sighup = IO::Async::Signal->new(
+   name => "HUP",
+   on_receipt => \&take_pic,
+);
+$loop->add($sighup);
+my $sigterm = IO::Async::Signal->new(
+   name => "TERM",
+   on_receipt => sub {
+	   $loop->stop;
+   },
+);
+$loop->add($sigterm);
+
+$loop->run();
+
+sub take_pic {
+    my $epoch = time();
+    system("/opt/vc/bin/raspistill -t 2000 -rot 270 -ex night -w -h -o ${epoch}cam.jpg");
+}
+
