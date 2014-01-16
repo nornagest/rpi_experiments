34a0c3b962d312bab757299db1aff9ee1969d201
Hagen Kühl <nornagest@gmx.de>
2014-01-16 11:18:53 +0100

    Create frame for module

---
+++ RPiManager/Module/Temperature.pm
@@ -0,0 +1,30 @@
+#
+#===============================================================================
+#
+# FILE: Temperature.pm
+#
+# DESCRIPTION: 
+#
+# FILES: ---
+# BUGS: ---
+# NOTES: ---
+# AUTHOR: Hagen Kuehl
+# ORGANIZATION:
+# VERSION: 1.0
+# CREATED: 01/16/2014 11:18:09 AM
+# REVISION: ---
+#===============================================================================
+
+package Module::Temperature;
+
+use Moose;
+
+use Modern::Perl 2013;
+use warnings;
+
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
