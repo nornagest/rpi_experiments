7049d6325f8332b7fdabad7e88d690341d29e81b
Hagen Kuehl <nornagest@gmx.de>
2014-01-22 20:02:33 +0100

    Minor adjustments at cam scripts.

--- cam_timelapse.sh
+++ cam_timelapse.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-DIR=/home/nornagest/cam
+DIR=/usr/share/nginx/img
 x=1
-while [ $x -le 60 ]; do                                                       
+while [ $x -le 132 ]; do                                                       
 filename=$(date -u +"%Y%m%d-%H%M-%S").jpg                                       
-raspistill -o $DIR/$filename -w 1280 -h 960 -n -t 1000
+raspistill -o $DIR/$filename -w 1280 -h 960 -rot 90 -n -t 1000
 x=$(( $x + 1 ))
 sleep 300;
 done
