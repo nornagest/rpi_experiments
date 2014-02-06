aca69383efe0ed6b9098a6be8d68426fe9b9de88
Hagen Kuehl <nornagest@gmx.de>
2014-02-06 20:17:39 +0100

    Get state from MPD

--- RPiManager/Module/Mpd.pm
+++ RPiManager/Module/Mpd.pm
@@ -40,13 +40,9 @@ has '__input_state' => ( is => 'rw', isa => 'Int', default => 0 );
 has 'song' => ( is => 'rw', isa => 'Str', default => '' );
 
 #TODO: Handle input as string
-#  add: next/prev adjust volume
-#TODO: check for MPD updates and print them
+#  add: next/prev and adjust volume
+#TODO: set __state depending on real state of MPD in sub ping()
 #  may have to use a routine for updates
-
-#add two timers:
-# 1: ping every 5s 
-# 2: check every second for status and print that / set state
 
 sub BUILD {
     my $self = shift;
@@ -72,11 +68,15 @@ override 'send' => sub {
 sub ping {
     my $self = shift;
     $self->__mpd->ping;
-    #my $status = $self->__mpd->update_status;
+
+    my $status = $self->__mpd->update_status;
+    my $state = $status->{"state"};
     my $song = $self->__mpd->current_song;
     my $name = $song->{"Name"};
-    if( $name ne $self->song) {
+
+    if( $name ne $self->song || $state ne $self->__state) {
         $self->song($name);
+        $self->__state($state);
         $self->print_state;
     }
 }
@@ -85,81 +85,29 @@ sub handle_input {
     my ($self, $byte) = @_;
 
     if( $byte == 1) {
-        if( $self->__state eq 'playing' ) {
+        if( $self->__state eq 'play' ) {
             $self->stop;
         } else {
             $self->play;
         }
-        #$self->__input_state(0);
-    }
-    return;
-
-    #shut off state machine for now
-    $self->state_0($byte) if $self->__input_state == 0;
-    $self->state_1($byte) if $self->__input_state == 1;
-    $self->state_2($byte) if $self->__input_state == 2;
-    $self->state_4($byte) if $self->__input_state == 4;
-}
-
-#TODO: make state machine a role, Manager too
-sub state_0 {
-    my ($self, $byte) = @_;
-    say "MPD: state_0 ", $self->__input_state, " ", $byte;
-    $self->__input_state(1) if $byte == 1;
-    $self->__input_state(2) if $byte == 2;
-    $self->__input_state(4) if $byte == 4;
-}
-
-sub state_1 {
-    my ($self, $byte) = @_;
-    say "MPD: state_1 ", $self->__input_state, " ", $byte;
-    if( $byte == 0) {
-        if( $self->__state eq 'playing' ) {
-            $self->stop;
-        } else {
-            $self->play;
-        }
-        $self->__input_state(0);
-    }
-}
-
-sub state_2 {
-    my ($self, $byte) = @_;
-    say "MPD: state_2 ", $self->__input_state, " ", $byte;
-    if( $byte == 0 ) {
-        $self->__input_state(0);
-    }
-}
-
-sub state_4 {
-    my ($self, $byte) = @_;
-    say "MPD: state_4 ", $self->__input_state, " ", $byte;
-    if( $byte == 0 ) {
-        $self->__input_state(0);
     }
 }
 
 sub play {
     my $self = shift;
     say "MPD: play ", $self->__state;
-    return if $self->__state eq 'playing';
+    return if $self->__state eq 'play';
     $self->__mpd->play;
-    $self->__state('playing');
-    $self->print_state;
 }
 sub stop {
     my $self = shift;
     say "MPD: stop ", $self->__state;
     $self->__mpd->stop;
-    $self->__state('stopped');
-    $self->print_state;
 }
 
 sub print_state {
     my $self = shift;
-
     my $output = $self->__state . ' Song: ' . $self->song;
-
     my $message = Message::Output->new(
         'Source' => $self->Name,
         'Content' => { 'string' => $output },
