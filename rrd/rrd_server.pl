038b27b03e877cbb06f6e1447504009de5646a1b
Hagen Kuehl <nornagest@kvoth.localdomain>
2015-04-22 21:09:30 +0200

    .

--- rrd/rrd_server.pl
+++ rrd/rrd_server.pl
@@ -102,8 +102,8 @@ $loop->listen(
             on_read => sub {
                 my ( $self, $buffref, $eof ) = @_;
                 return 0 unless $eof;
-                my $message = thaw($$buffref) 
-                    if eval { $message = thaw($$buffref) };
+                my $message; 
+                return unless eval { $message = thaw($$buffref) };
                 save_data($$message);
                 $$buffref = "";
             },
