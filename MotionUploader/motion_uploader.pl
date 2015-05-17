4553012f5fc9a8fb2ca533d40bff777952149937
Hagen Kuehl <nornagest@gmx.de>
2015-05-17 16:21:46 +0200

    Changed Uploader

--- MotionUploader/motion_uploader.pl
+++ MotionUploader/motion_uploader.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/env perl 
 #===============================================================================
 #
-#         FILE: MotionUploader.pl
+#         FILE: rrd_uploader.pl
 #
-#        USAGE: ./MotionUploader.pl  
+#        USAGE: ./rrd_uploader.pl  
 #
 #  DESCRIPTION: 
 #
@@ -24,32 +24,38 @@ use File::Find;
 use Sys::Hostname;
 
 my $host = hostname;
+
 my $rsync_source = "/usr/share/motion/";
 my $rsync_dest = "nornagest\@nornagest.org:/usr/share/images/" . $host;
 my $rsync_command = "rsync -r --remove-source-files";
-my @jpgs;
+
+my @imgs;
+my $count;
 
 while(1) {
-    upload_files() if check_files();
-    sleep(60);
+    upload_files() if check_files() && scalar @imgs == $count;
+    sleep(5);
 }
 
 sub check_files {
-    @jpgs = ();
+    $count = scalar @imgs;
+    @imgs = ();
+    sleep(5);
     find(\&wanted, $rsync_source);
-    return scalar @jpgs;
+    return scalar @imgs;
 }
 
 sub wanted {
-    /.*\.jpg/ && !/lastsnap\.jpg/ && push @jpgs, $_;
+    /.*\.jpg/ && !/lastsnap\.jpg/ && push @imgs, $_;
 }
 
 sub upload_files {
     my $command = $rsync_command . " " . $rsync_source . "* " . $rsync_dest;
 
-    #say scalar localtime . " Executing: $command";
-    say scalar localtime . " Starting upload of " . scalar @jpgs . " files...";
+    say scalar localtime . " Starting upload of " . $count . " files...";
     system($command);
     say scalar localtime . " Done.";
+    
+    sleep(60);
 }
 
